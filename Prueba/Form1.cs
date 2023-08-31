3fb278880642a0d4cb67036a307c0e10112e7fc9
DS22005 <DS22005@ti.ues.edu.sv>
2023-08-31 17:29:42 -0600

    Otros cambios realizados

--- Prueba/Form1.cs
+++ Prueba/Form1.cs
@@ -26,5 +26,10 @@ namespace Prueba
         {
             Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
