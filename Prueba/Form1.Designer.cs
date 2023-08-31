788966dd667fa3e790471699dbd8bb2c8552537a
DS22005 <DS22005@ti.ues.edu.sv>
2023-08-31 17:11:19 -0600

    Primer cambio

---
+++ Prueba/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNopuedo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNopuedo
+            // 
+            this.lblNopuedo.AutoSize = true;
+            this.lblNopuedo.Location = new System.Drawing.Point(46, 42);
+            this.lblNopuedo.Name = "lblNopuedo";
+            this.lblNopuedo.Size = new System.Drawing.Size(54, 13);
+            this.lblNopuedo.TabIndex = 0;
+            this.lblNopuedo.Text = "No puedo";
+            this.lblNopuedo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNopuedo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNopuedo;
+    }
+}
+
