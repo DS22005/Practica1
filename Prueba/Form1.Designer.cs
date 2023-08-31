996a762d5dc0b030c0c71fb1c7f2804b72325f5b
Oscar Ernesto Sarceño Galdamez <sg21043@ues.edu.sv>
2023-08-31 17:40:15 -0600

    boton

--- Prueba/Form1.Designer.cs
+++ Prueba/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Prueba
             this.lblNopuedo = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNopuedo
@@ -46,7 +47,7 @@ namespace Prueba
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(368, 93);
+            this.button1.Location = new System.Drawing.Point(84, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(198, 50);
             this.button1.TabIndex = 1;
@@ -56,7 +57,7 @@ namespace Prueba
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(116, 139);
+            this.button2.Location = new System.Drawing.Point(372, 73);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 38);
             this.button2.TabIndex = 2;
@@ -64,11 +65,21 @@ namespace Prueba
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 122);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "xd";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblNopuedo);
@@ -84,6 +95,7 @@ namespace Prueba
         private System.Windows.Forms.Label lblNopuedo;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
