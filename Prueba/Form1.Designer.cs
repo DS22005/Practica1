5288ec5ab5a26b0b8bd1c631ffc3572e1bbe9c25
FranciscoAsBer <AB11029@ti.ues.edu.sv>
2023-09-03 12:39:39 -0600

    prueba 23

--- Prueba/Form1.Designer.cs
+++ Prueba/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace Prueba
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNopuedo
@@ -84,11 +85,22 @@ namespace Prueba
             this.label1.TabIndex = 4;
             this.label1.Text = "aqui esta otro";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(209, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 74);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -108,6 +120,7 @@ namespace Prueba
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
